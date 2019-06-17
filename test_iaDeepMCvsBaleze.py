2cccc482ae0389914157fc86dc8c2c62470125b4
AnthonyPereira <45620484+AnthonyPereira@users.noreply.github.com>
2019-06-17 16:21:43 +0200

    Delete test_iaDeepMCvsBaleze.py

--- test_iaDeepMCvsBaleze.py
+++
@@ -1,9 +0,0 @@
-import DeepMCvsBaleze
-
-state = ()
-
-while True:
-    action = DeepMCvsBaleze.nextAction(0, state)
-    state = state + (action,)
-    print(state)
-    state = state + (int(input()),)
